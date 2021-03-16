ee1da12cb4599d28c6d89907573f4578f6bf573c
DESKTOP-3U3A5NC\rober <robert.mccahil@gmail.com>
2021-03-16 16:31:58 +0000

    Add movement script, collision toggle script

---
+++ Assets/Project2/Scripts/Mechanics/MovableObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    Vector3 start;
+    public Vector3 end;
+    public float moveSpeed = 1f;
+    public bool reverseAndRepeat = false;
+
+    private bool isReversing = false;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        start = transform.position;
+    }
+
+    void Update()
+    {
+        if (isMoving)
+            return;
+
+        if (!isReversing)
+        {
+            StartCoroutine(MoveToPosition(transform, end));
+        }
+        else
+        {
+            StartCoroutine(MoveToPosition(transform, start));
+        }
+
+        if (reverseAndRepeat)
+            isReversing = !isReversing;
+    }
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position)
+    {
+        isMoving = true;
+        var currentPos = transform.position;
+        var t = 0f;
+        while (t < 1 && enabled)
+        {
+            t += Time.deltaTime * Mathf.Max(moveSpeed, 0.1f);
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
---
+++ Assets/Project2/Scripts/Mechanics/OnCollisionScriptToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionScriptToggle : MonoBehaviour
+{
+    public bool alwaysToggleToTrue = true;
+    public List<MonoBehaviour> scriptsToEnable;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (var script in scriptsToEnable) 
+        {
+            if (alwaysToggleToTrue)
+            {
+                script.enabled = true;
+            }
+            else
+            {
+                script.enabled = !script.enabled;
+            }
+        }
+    }
+}
