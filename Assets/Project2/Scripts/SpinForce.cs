d8cf08cd9a0847a2d89604392b395a6a04fe1a3b
Philip-Waldron <philip_waldron@yahoo.com>
2021-03-17 16:41:02 +0000

    Add SpinForce script

---
+++ Assets/Project2/Scripts/SpinForce.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SpinForce : MonoBehaviour
+{
+    public Rigidbody Rigidbody;
+    public Vector3 Torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Rigidbody == null)
+        {
+            Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        Rigidbody.AddTorque(Torque, ForceMode.Impulse);
+    }
+}
