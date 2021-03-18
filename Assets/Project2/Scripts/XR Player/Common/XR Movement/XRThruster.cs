a7b89a23a83b8a81889af936798543466854a19a
Ciaran Trotman <ciarantrotman@gmail.com>
2021-03-18 21:16:47 +0000

    Better Thrusters

--- Assets/Project2/Scripts/XR Player/Common/XR Movement/XRThruster.cs
+++ Assets/Project2/Scripts/XR Player/Common/XR Movement/XRThruster.cs
@@ -8,20 +8,79 @@ namespace Project2.Scripts.XR_Player.Common.XR_Movement
     {
         [SerializeField] private Rigidbody playerRigidbody;
         [SerializeField, Range(0, 50)] private float force;
+        [SerializeField, Range(1, 5)] private float duration;
+
+        private Thruster left, right;
         
-        private void FixedUpdate()
+        [Serializable] public class Thruster
         {
-            Thruster(XRInputController.Check.Right);
-            Thruster(XRInputController.Check.Left);
+            private XRInputController.Check check;
+            private Rigidbody rigidbody;
+            private float force, duration;
+            private bool thrusting;
+
+            private float thrustStartTime;
+            private bool TimeOut => Time.time - thrustStartTime >= duration;
+            private float Value => (Time.time - thrustStartTime) / duration;
+            
+            public void SetupThruster(XRInputController.Check setCheck, Rigidbody setRigidbody, float setForce, float setDuration)
+            {
+                check = setCheck;
+                rigidbody = setRigidbody;
+                force = setForce;
+                duration = setDuration;
+            }
+            
+            public void ThrustLogic(float setForce, float setDuration)
+            {
+                force = setForce;
+                duration = setDuration;
+                
+                if (thrusting)
+                {
+                    Thrust();
+                    if (TimeOut || ThrustEnd())
+                    {
+                        thrusting = false;
+                    }
+                }
+                if (ThrustStart())
+                {
+                    thrustStartTime = Time.time;
+                    thrusting = true;
+                }
+            }
+
+            private void Thrust()
+            {
+                rigidbody.AddForce(XRInputController.Forward(check) * force);
+                Debug.DrawRay(XRInputController.Position(check), XRInputController.Forward(check) * force, Color.yellow);
+                Debug.DrawRay(XRInputController.Position(check), -XRInputController.Forward(check) * (1 - Value), Color.green);
+            }
+
+            private bool ThrustStart()
+            {
+                return !thrusting && XRInputController.InputEvent(XRInputController.XRControllerButton.Trigger).State(check, XRInputController.InputEvents.InputEvent.Transition.Down);
+            }
+            
+            private bool ThrustEnd()
+            {
+                return (thrusting && XRInputController.InputEvent(XRInputController.XRControllerButton.Trigger).State(check, XRInputController.InputEvents.InputEvent.Transition.Up));
+            }
         }
 
-        private void Thruster(XRInputController.Check check)
+        private void Awake()
         {
-            if (XRInputController.ControllerButton(XRInputController.XRControllerButton.Trigger, check))
-            {
-                playerRigidbody.AddForce(XRInputController.Forward(check) * force);
-                Debug.DrawRay(XRInputController.Position(check), XRInputController.Forward(check) * force, Color.yellow);
-            }
+            left = new Thruster();
+            left.SetupThruster(XRInputController.Check.Left, playerRigidbody, force, duration);
+            right = new Thruster();
+            right.SetupThruster(XRInputController.Check.Right, playerRigidbody, force, duration);
+        }
+
+        private void FixedUpdate()
+        {
+            left.ThrustLogic(force, duration);
+            right.ThrustLogic(force, duration);
         }
     }
 }
